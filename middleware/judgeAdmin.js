845f2891bdc46e257cf230ef818c27964801d227
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-12-22 09:27:49 +0900

    DASH31_閲覧制限のミドルウェアを用意
    * 管理者のみ、管理者機能ページを閲覧可に設定

---
+++ middleware/judgeAdmin.js
@@ -0,0 +1,9 @@
+export default function ({ store, redirect, route }) {
+
+  // role.jsのroleステートがadminかつ、ルーティングにadminが含まれていればログイン画面にリダイレクト
+  if (store.state.role.role != 'admin' && route.path.indexOf('admin') > -1 ){
+    console.log(route.path.indexOf('admin'));
+    alert('アクセスする権限がありません');
+    redirect('/login');
+  }
+}
